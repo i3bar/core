28f2192316bf3aa0e1728ea5e4498dbd231b4e8b
Amin NAIRI <nairi.amin@gmail.com>
2019-08-10 14:11:49 +0200

    :memo: added gplv3 licensing

--- sources/type.js
+++ sources/type.js
@@ -1,4 +1,23 @@
 /**
+ *
+ * Core library for creating i3bar using Node.js.
+ * Copyright (C) 2019 Amin NAIRI
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * @param {unknown} input
  * @return {string}
  */
