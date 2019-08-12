e9cca87563720da782933598139fcc1ccbcf89f8
Amin NAIRI <nairi.amin@gmail.com>
2019-08-12 19:14:34 +0200

    :sparkles: added some more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,73 @@ module.exports = {
     "semi": [
       "error",
       "always"
+    ],
+    "for-direction": [
+      "error"
+    ],
+    "getter-return": [
+      "error",
+      {
+        "allowImplicit": false
+      }
+    ],
+    "no-async-promise-executor": [
+      "error"
+    ],
+    "no-await-in-loop": [
+      "error"
+    ],
+    "no-compare-neg-zero": [
+      "error"
+    ],
+    "no-cond-assign": [
+      "error",
+      "always"
+    ],
+    "no-console": [
+      "error",
+    ],
+    "no-constant-condition": [
+      "error",
+      {
+        "checkLoops": false
+      }
+    ],
+    "no-control-regex": [
+      "error"
+    ],
+    "no-debugger": [
+      "error"
+    ],
+    "no-dupe-args": [
+      "error"
+    ],
+    "no-dupe-keys": [
+      "error"
+    ],
+    "no-duplicate-case": [
+      "error"
+    ],
+    "no-empty": [
+      "error"
+    ],
+    "no-empty-character-class": [
+      "error"
+    ],
+    "no-ex-assign": [
+      "error"
+    ],
+    "no-extra-boolean-cast": [
+      "error"
+    ],
+    "no-extra-parens": [
+      "error"
+    ],
+    "no-extra-semi": [
+      "error"
+    ],
+    "no-func-assign": [
+      "error"
     ]
   }
 };
