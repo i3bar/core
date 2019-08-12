0523df75c84dc6ff67ea0f9e924184fff9b530dd
Amin NAIRI <nairi.amin@gmail.com>
2019-08-12 22:27:46 +0200

    :wrench: added some more rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+const INDENTATION_SPACES_COUNT = 2;
+const PARSER_ECMASCRIPT_VERSION = 2018;
+
 module.exports = {
   "env": {
     "es6": true,
@@ -9,13 +12,13 @@ module.exports = {
     "SharedArrayBuffer": "readonly"
   },
   "parserOptions": {
-    "ecmaVersion": 2018,
+    "ecmaVersion": PARSER_ECMASCRIPT_VERSION,
     "sourceType": "module"
   },
   "rules": {
     "indent": [
       "error",
-      2
+      INDENTATION_SPACES_COUNT
     ],
     "linebreak-style": [
       "error",
@@ -94,6 +97,221 @@ module.exports = {
       "error"
     ],
     "no-func-assign": [
+      "error"
+    ],
+    "no-inner-declarations": [
+      "error",
+      "both"
+    ],
+    "no-invalid-regexp": [
+      "error"
+    ],
+    "no-irregular-whitespace": [
+      "error",
+      {
+        "skipStrings": false,
+        "skipComments": false,
+        "skipRegExps": false,
+        "skipTemplates": false
+      }
+    ],
+    "no-misleading-character-class": [
+      "error"
+    ],
+    "no-obj-calls": [
+      "error"
+    ],
+    "no-prototype-builtins": [
+      "error"
+    ],
+    "no-regex-spaces": [
+      "error"
+    ],
+    "no-sparse-arrays": [
+      "error"
+    ],
+    "no-template-curly-in-string": [
+      "error"
+    ],
+    "no-unexpected-multiline": [
+      "error"
+    ],
+    "no-unreachable": [
+      "error"
+    ],
+    "no-unsafe-finally": [
+      "error"
+    ],
+    "no-unsafe-negation": [
+      "error"
+    ],
+    "require-atomic-updates": [
+      "error"
+    ],
+    "use-isnan": [
+      "error"
+    ],
+    "valid-typeof": [
+      "error"
+    ],
+    "accessor-pairs": [
+      "error",
+      {
+        "setWithoutGet": false,
+        "getWithoutSet": false
+      }
+    ],
+    "array-callback-return": [
+      "error",
+      {
+        "allowImplicit": false
+      }
+    ],
+    "block-scoped-var": [
+      "error"
+    ],
+    "class-methods-use-this": [
+      "error"
+    ],
+    "complexity": [
+      "off"
+    ],
+    "consistent-return": [
+      "error",
+      {
+        "treatUndefinedAsUnspecified": true
+      }
+    ],
+    "curly": [
+      "error",
+      "all"
+    ],
+    "default-case": [
+      "error",
+      {
+        "commentPattern": "^Skip\\sdefault$"
+      }
+    ],
+    "dot-location": [
+      "error",
+      "property"
+    ],
+    "dot-notation": [
+      "error",
+      {
+        "allowKeywords": false
+      }
+    ],
+    "eqeqeq": [
+      "error",
+      "always"
+    ],
+    "guard-for-in": [
+      "error"
+    ],
+    "max-classes-per-file": [
+      "off"
+    ],
+    "no-alert": [
+      "error"
+    ],
+    "no-caller": [
+      "error"
+    ],
+    "no-case-declarations": [
+      "error"
+    ],
+    "no-div-regex": [
+      "error"
+    ],
+    "no-else-return": [
+      "error"
+    ],
+    "no-empty-function": [
+      "error"
+    ],
+    "no-empty-pattern": [
+      "error"
+    ],
+    "no-eq-null": [
+      "error"
+    ],
+    "no-eval": [
+      "error",
+      {
+        "allowIndirect": false
+      }
+    ],
+    "no-extend-native": [
+      "error"
+    ],
+    "no-extra-bind": [
+      "error"
+    ],
+    "no-extra-label": [
+      "error"
+    ],
+    "no-fallthrough": [
+      "error"
+    ],
+    "no-floating-decimal": [
+      "error"
+    ],
+    "no-global-assign": [
+      "error"
+    ],
+    "no-implicit-coercion": [
+      "error"
+    ],
+    "no-implicit-globals": [
+      "error"
+    ],
+    "no-implied-eval": [
+      "error"
+    ],
+    "no-invalid-this": [
+      "error"
+    ],
+    "no-iterator": [
+      "error"
+    ],
+    "no-labels": [
+      "error",
+      {
+        "allowLoop": false,
+        "allowSwitch": false
+      }
+    ],
+    "no-lone-blocks": [
+      "error"
+    ],
+    "no-loop-func": [
+      "error"
+    ],
+    "no-magic-numbers": [
+      "error",
+      {
+        "ignoreArrayIndexes": false,
+        "enforceConst": true,
+        "detectObjects": true
+      }
+    ],
+    "no-multi-spaces": [
+      "error",
+      {
+        "ignoreEOLComments": false
+      }
+    ],
+    "no-multi-str": [
+      "error"
+    ],
+    "no-new": [
+      "error"
+    ],
+    "no-new-func": [
+      "error"
+    ],
+    "no-new-wrappers": [
       "error"
     ]
   }
